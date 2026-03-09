94d4a5a43114e77db0900536b5cfb91198164b0d
Gayathridevi G <gopalakrishnangayathri589@gmail.com>
2026-03-09 18:08:54 +0530

    Refactor model loading and improve input labels

--- streamlit_app_ml/app.py
+++ streamlit_app_ml/app.py
@@ -2,21 +2,25 @@ import streamlit as st
 import pickle
 import os
 
+# Get current directory of app.py
 current_dir = os.path.dirname(__file__)
+
+# Build full path to model
 model_path = os.path.join(current_dir, "model.pkl")
 
-model = pickle.load(open(model_path, "rb"))
+# Load model
+with open(model_path, "rb") as file:
+    model = pickle.load(file)
 
 st.title("Iris Flower Prediction App")
 
-sepal_length = st.number_input("SepalLength")
-sepal_width = st.number_input("SepalWidth")
-petal_length = st.number_input("PetalLength")
-petal_width = st.number_input("PetalWidth")
+sepal_length = st.number_input("Sepal Length")
+sepal_width = st.number_input("Sepal Width")
+petal_length = st.number_input("Petal Length")
+petal_width = st.number_input("Petal Width")
 
 if st.button("Predict"):
 
-    prediction = model.predict([[sepal_length,sepal_width,petal_length,petal_width]])
-
+    prediction = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
 
     st.success(f"Predicted Flower: {prediction[0]}")
